151cce8d8208480a54336da9fc74c6143c6562af
zhliu2018 <lzhpersonal@163.com>
2019-03-06 14:42:06 +0800

    delete

--- demo.py
+++
@@ -1,8 +0,0 @@
-import os
-from PIL import Image
-import matplotlib.pyplot as  plt
-
-image = Image.open('/home/lzhpersonal/darknet/data/img/0101_1857_001_left.jpg')
-img = image.crop((1000,100,5000,5000))
-plt.imshow(img)
-plt.show()
